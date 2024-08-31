e35dddcf9b075fb6e28d3e8d49ef6a89e7264aca
Deddy Kakunsi <deddy.kakunsi@tech.jago.com>
2024-08-31 10:48:34 +0800

    Update release script and add actions workflow.

--- release.sh
+++ release.sh
@@ -1,12 +1,18 @@
 #! /bin/sh
 
+release_solution() {
+  CHART_VERSION=${1}
+  helm pull oci://ghcr.io/cortechdigitallab/helm/reference-iam --version $CHART_VERSION
+  helm pull oci://ghcr.io/cortechdigitallab/helm/reference-data --version $CHART_VERSION
+  helm pull oci://ghcr.io/cortechdigitallab/helm/reference-process --version $CHART_VERSION
+  helm pull oci://ghcr.io/cortechdigitallab/helm/reference-gateway --version $CHART_VERSION
+}
+
 cd ./charts
 
-CHART_VERSION=${1}
-helm pull oci://ghcr.io/cortechdigitallab/helm/reference/iam --version $CHART_VERSION
-helm pull oci://ghcr.io/cortechdigitallab/helm/reference/data --version $CHART_VERSION
-helm pull oci://ghcr.io/cortechdigitallab/helm/reference/process --version $CHART_VERSION
-helm pull oci://ghcr.io/cortechdigitallab/helm/reference/gateway --version $CHART_VERSION
+if [ "$1" == solution ]; then
+  release_solution $2
+fi
 
 cd ..
 helm repo index .
